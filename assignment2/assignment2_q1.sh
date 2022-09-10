0b12d38743b43377ceef4643e3dfcaf054fd2a40
Ridhiman Dhindsa <93422234+rdhkdh@users.noreply.github.com>
2022-09-10 16:02:19 +0530

    lab_trial q1

--- assignment2/assignment2_q1.sh
+++ assignment2/assignment2_q1.sh
@@ -4,6 +4,48 @@
 
 #!/bin/bash
 
-while read -r filename destn_dir; do        #destination directory
-    cp -v /home/$USER/$filename /home/$USER/backup_folder/
-done <"backup_files.txt"
+#checking if each parameter is passed or not
+
+if [ -z "$1" ]; then
+	echo "You have failed to pass argument 1: Source File name."
+	exit 255; #process exited with failure
+fi
+
+if [ -z "$2" ]; then
+	echo "You have failed to pass argument2: Destination Directory name."
+	exit 255; 
+fi
+
+if[ $#>2 ]; then
+echo "ERROR: Too many parameters passed."
+exit 255;
+
+#checking if parameters passed, exist or not
+
+if [ -f "$1" ]; then
+    echo "VERIFICATION COMPLETE: Source File passed as argument exists." 
+else
+    echo "ERROR: Source File does not exist."
+    exit 255; 
+fi
+
+if [ -d "/home/$USER/$2" ]; then
+    echo "VERIFICATION COMPLETE: Destination Directory passed as argument exists." 
+else
+    echo "ERROR: Destination Directory does not exist."
+    exit 255; 
+fi
+
+#Carrying out backup process once verification is complete:
+
+while read -r filename; do        
+    cp -v /home/$USER/$filename /home/$USER/$2/  #backup_folder -destination directory
+    echo "created backup for $filename"
+    echo
+done <"$1" #files_for_backup.txt -list of files for backup
+
+
+#things to add:
+# log file?
+# timestamp?
+# reminder?:: echo "REMINDER: All required files will be copied to /home/$USER/destination_directory"
