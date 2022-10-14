b0de6a742dc3c945af82c355486bac7a1abf50e3
Ridhiman <rdhkdh21@gmail.com>
2022-10-14 17:56:32 +0530

    assgn 3

--- assignment3_final/assgn3_q2_github_code.py
+++ assignment3_final/assgn3_q2_github_code.py
@@ -1,3 +1,6 @@
+# Programming Assignment 3, Question 2
+# Ridhiman Kaur Dhindsa, Roll no.210101088
+
 from copy import deepcopy
 import numpy as np
 import time
@@ -28,19 +31,7 @@ def manhattan(puzzle, goal):
     b = abs(puzzle % 3 - goal % 3)
     mhcost = a + b
     return sum(mhcost[1:])
-
-
-
-
-# will calcuates the number of misplaced tiles in the current state as compared to the goal state
-def misplaced_tiles(puzzle,goal):
-    mscost = np.sum(puzzle != goal) - 1
-    return mscost if mscost > 0 else 0
-       
-
-
-#3[on_true] if [expression] else [on_false] 
-
+ 
 
 # will indentify the coordinates of each of goal or initial state values
 def coordinates(puzzle):
@@ -50,53 +41,47 @@ def coordinates(puzzle):
     return pos
 
 
-
 # start of 8 puzzle evaluation, using Manhattan heuristics 
-def evaluvate(puzzle, goal):
-    steps = np.array([('up', [0, 1, 2], -3),('down', [6, 7, 8],  3),('left', [0, 3, 6], -1),('right', [2, 5, 8],  1)],
-                dtype =  [('move',  str, 1),('position', list),('head', int)])
-
+def evaluate(puzzle, goal):
+    steps = np.array([('up', [0, 1, 2], -3),('down', [6, 7, 8],  3),('left', [0, 3, 6], -1),('right', [2, 5, 8],  1)], dtype =  [('move',  str, 1),('position', list),('head', int)])
     dtstate = [('puzzle',  list),('parent', int),('gn',  int),('hn',  int)]
     
-     # initializing the parent, gn and hn, where hn is manhattan distance function call 
-    costg = coordinates(goal)
-    parent = -1
-    gn = 0
-    hn = manhattan(coordinates(puzzle), costg)
+    # initializing the parent, gn and hn, where hn is manhattan distance function call 
+    parent = -1  # No parent of root, so initially parent= -1
+    gn = 0   # Depth of root is 0, therefore initially g(n)=0
+    hn = manhattan(coordinates(puzzle), coordinates(goal))
     state = np.array([(puzzle, parent, gn, hn)], dtstate)
 
 # We make use of priority queues with position as keys and fn as value.
     dtpriority = [('position', int),('fn', int)]
     priority = np.array( [(0, hn)], dtpriority)
 
-
-
     while 1:
         priority = np.sort(priority, kind='mergesort', order=['fn', 'position'])     
         position, fn = priority[0]                                                 
         priority = np.delete(priority, 0, 0)  
-        # sort priority queue using merge sort,the first element is picked for exploring remove from queue what we are exploring                   
+        # sort priority queue using merge sort,the first element is picked for exploring, remove from queue what we are exploring                   
         puzzle, parent, gn, hn = state[position]
         puzzle = np.array(puzzle)
         # Identify the blank square in input 
         blank = int(np.where(puzzle == 0)[0])       
         gn = gn + 1                              
         c = 1
-        start_time = time.time()
+        start_time = time.time() 
         for s in steps:
             c = c + 1
             if blank not in s['position']:
                 # generate new state as copy of current
-                openstates = deepcopy(puzzle)                   
+                openstates = deepcopy(puzzle)  # openstates is a list of nodes that are currently being explored.                   
                 openstates[blank], openstates[blank + s['head']] = openstates[blank + s['head']], openstates[blank]             
                 # The all function is called, if the node has been previously explored or not
                 if ~(np.all(list(state['puzzle']) == openstates, 1)).any():    
                     end_time = time.time()
                     if (( end_time - start_time ) > 2):
-                        print(" The 8 puzzle is unsolvable ! \n")
+                        print(" The 8 puzzle is unsolvable ! \n") # process is deemed unsolvable if it takes more than 2 minutes.
                         exit 
                     # calls the manhattan function to calcuate the cost 
-                    hn = manhattan(coordinates(openstates), costg)    
+                    hn = manhattan(coordinates(openstates), coordinates(goal))    
                      # generate and add new state in the list                    
                     q = np.array([(openstates, position, gn, hn)], dtstate)         
                     state = np.append(state, q, 0)
@@ -132,18 +117,15 @@ for i in range(0,9):
     goal.append(x)
 
 # evaluation using manhattan distance: 
-state, visited = evaluvate(puzzle, goal) 
+state, visited = evaluate(puzzle, goal) 
+
 bestpath = bestsolution(state)
 print(str(bestpath).replace('[', ' ').replace(']', ''))
+
 totalmoves = len(bestpath) - 1
 print('Steps to reach goal:',totalmoves)
+
 visit = len(state) - visited
 print('Total nodes visited: ',visit, "\n")
+
 print('Total generated:', len(state))
-
-    
-
-    
-    
-
-
---
+++ assignment3_final/assignment3_q1.py
@@ -0,0 +1,42 @@
+# Programming Assignment 3, Question 1
+# Ridhiman Kaur Dhindsa, Roll no.210101088
+
+print("\n     *** Josephus Problem ***       \n")
+
+n=input("Enter n: ") #takes string input
+n=int(n) #have to convert n to integer
+while(n>26):
+    n=input("ERROR in n value. Enter n less than 26: ")
+    n=int(n)
+
+k= input("Enter k: ") 
+k=int(k) #have to convert k to integer
+
+
+
+def increment(x,amt,y):
+    x=(x+amt)%y
+    return x
+
+list1= []
+
+for i in range(0,n):
+    list1.append(chr(i+65))
+
+print("List of people:")
+print(list1)
+print("\nOrder of execution:")
+
+index=0
+
+while(len(list1)!=1):
+    index= increment(index,k,n)
+    print (list1[index])
+    del list1[index]
+    n=n-1
+
+print()
+print(list1[0], "survives.\n")
+
+print()
+print("     *** End of Program ***      \n")
